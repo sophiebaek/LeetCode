0b5d51fe373b01c4b9e32cb5482c48fec119915c
Jiyeon Baek <54691854+sophiebaek@users.noreply.github.com>
2020-12-26 21:30:44 +0900

    Update maximum_depth_of_binary_tree.py

--- Python/maximum_depth_of_binary_tree.py
+++ Python/maximum_depth_of_binary_tree.py
@@ -9,6 +9,18 @@ class Solution(object):
         """
         :type root: TreeNode
         :rtype: int
+        
+        >>> Input: root = [3,9,20,null,null,15,7]
+        Output: 3
+        
+        >>> Input: root = [1,null,2]
+        Output: 2
+        
+        >>> Input: root = []
+        Output: 0
+        
+        >>> Input: root = [0]
+        Output: 1
         """
         
         if root == None:
