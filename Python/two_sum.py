e6bace78c4af57bb73427fa7daa89f69c3276374
Jiyeon Baek <54691854+sophiebaek@users.noreply.github.com>
2020-12-26 21:33:58 +0900

    Update two_sum.py

--- Python/two_sum.py
+++ Python/two_sum.py
@@ -4,6 +4,15 @@ class Solution(object):
         :type nums: List[int]
         :type target: int
         :rtype: List[int]
+        
+        >>> Input: nums = [2,7,11,15], target = 9
+        Output: [0,1]
+        
+        >>> Input: nums = [3,2,4], target = 6
+        Output: [1,2]
+        
+        >>> Input: nums = [3,3], target = 6
+        Output: [0,1]
         """
         sum_value = 0
         
