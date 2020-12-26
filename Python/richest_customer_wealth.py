407116115cd04aa55598d8ffb7496da82c18c6bd
Jiyeon Baek <54691854+sophiebaek@users.noreply.github.com>
2020-12-26 21:27:30 +0900

    Update richest_customer_wealth.py

--- Python/richest_customer_wealth.py
+++ Python/richest_customer_wealth.py
@@ -3,6 +3,15 @@ class Solution(object):
         """
         :type accounts: List[List[int]]
         :rtype: int
+        
+        >>> Input: accounts = [[1,2,3],[3,2,1]]
+        Output: 6
+        
+        >>> Input: accounts = [[1,5],[7,3],[3,5]]
+        Output: 10
+        
+        >>> Input: accounts = [[2,8,7],[7,1,3],[1,9,5]]
+        Output: 17
         """
         
         max_wealth = 0
