9ae3fa9e72385d3a15ed20f2c5e9d7cac39a706e
Jiyeon Baek <54691854+sophiebaek@users.noreply.github.com>
2020-12-26 21:31:45 +0900

    Update running_sum_of_1d_array.py

--- Python/running_sum_of_1d_array.py
+++ Python/running_sum_of_1d_array.py
@@ -3,6 +3,15 @@ class Solution(object):
         """
         :type nums: List[int]
         :rtype: List[int]
+        
+        >>> Input: nums = [1,2,3,4]
+        Output: [1,3,6,10]
+        
+        >>> Input: nums = [1,1,1,1,1]
+        Output: [1,2,3,4,5]
+        
+        >>> Input: nums = [3,1,2,10,1]
+        Output: [3,4,6,16,17]
         """
         
         if len(nums) == 0:
