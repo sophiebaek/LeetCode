b3142e96cac39c489c3b4ffa82e66fe909f8d2bf
Jiyeon Baek <54691854+sophiebaek@users.noreply.github.com>
2021-01-10 00:55:40 +0900

    Update swap_salary.sql

--- MySQL/swap_salary.sql
+++ MySQL/swap_salary.sql
@@ -1,5 +1,23 @@
 # Write your MySQL query statement below
 
+"""
+Given a table salary:
+| id | name | sex | salary |
+|----|------|-----|--------|
+| 1  | A    | m   | 2500   |
+| 2  | B    | f   | 1500   |
+| 3  | C    | m   | 5500   |
+| 4  | D    | f   | 500    |
+
+After update:
+| id | name | sex | salary |
+|----|------|-----|--------|
+| 1  | A    | f   | 2500   |
+| 2  | B    | m   | 1500   |
+| 3  | C    | f   | 5500   |
+| 4  | D    | m   | 500    |
+"""
+
 UPDATE
     salary
 SET
