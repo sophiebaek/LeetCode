b6189465eee22e85660e93c9997c72892ac98b9a
Jiyeon Baek <54691854+sophiebaek@users.noreply.github.com>
2021-01-12 04:40:21 +0900

    Create not_boring_movies.sql

---
+++ MySQL/not_boring_movies.sql
@@ -0,0 +1,28 @@
+# Write your MySQL query statement below
+
+'''
+Table cinema:
++---------+-----------+--------------+-----------+
+|   id    | movie     |  description |  rating   |
++---------+-----------+--------------+-----------+
+|   1     | War       |   great 3D   |   8.9     |
+|   2     | Science   |   fiction    |   8.5     |
+|   3     | irish     |   boring     |   6.2     |
+|   4     | Ice song  |   Fantacy    |   8.6     |
+|   5     | House card|   Interesting|   9.1     |
++---------+-----------+--------------+-----------+
+
+The output should be:
++---------+-----------+--------------+-----------+
+|   id    | movie     |  description |  rating   |
++---------+-----------+--------------+-----------+
+|   5     | House card|   Interesting|   9.1     |
+|   1     | War       |   great 3D   |   8.9     |
++---------+-----------+--------------+-----------+
+'''
+
+SELECT *
+FROM cinema
+WHERE description != "boring" AND mod(id, 2) = 1
+order by rating DESC
+;
